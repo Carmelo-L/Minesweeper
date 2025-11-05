215c3ff9fed8351827b2e31f7a8308e29f6bc0ad
Carmelo <carmeloLabbozzetta@student.unsw.edu.au>
2025-11-05 17:00:52 +1100

    began adding tile and flag logic

---
+++ scripts/tile.gd
@@ -0,0 +1,29 @@
+extends TextureButton
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+const LEFT_CLICK: int 	= 1
+const RIGHT_CLICK: int 	= 2
+const VISIBLE: bool		= true
+const INVISIBLE: bool	= false
+
+@onready var flag: Sprite2D = $Flag
+var clickable: bool = true
+
+func _button_press() -> void:
+	if clickable:
+		disabled = true
+
+func _add_or_remove_flag() -> void:
+	if flag.visible:
+		flag.visible = INVISIBLE
+		clickable = true
+	else:
+		flag.visible = VISIBLE
+		clickable = false
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event.button_mask == LEFT_CLICK:
+		_button_press()
+	if event.button_mask == RIGHT_CLICK:
+		_add_or_remove_flag()
